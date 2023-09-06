6478a97a720250a0241b0de730769ef5da0ba6de
Amy Miles <33338870+amy-miles@users.noreply.github.com>
2023-09-05 22:18:09 -0500

    Created the ContactList Database steps 12-15

---
+++ Multi-PageWevAppwDB/Migrations/20230906031342_Initial.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Multi_PageWevAppwDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    ContactID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.ContactID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "ContactID", "Address", "Name", "Note", "PhoneNumber" },
+                values: new object[,]
+                {
+                    { 1, "144 Para St.", "Colin Robinson", "What We Do In the Shadows", "1234567890" },
+                    { 2, "126 Kellum Ct.", "Michael Scott", "The Office", "1234567890" },
+                    { 3, "11238 Jousten Ave. Apt. 6E", "Detective Jake Peralta", "Brooklyn 99", "1234567890" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
--- Multi-PageWevAppwDB/Models/Contact.cs
+++ Multi-PageWevAppwDB/Models/Contact.cs
@@ -1,6 +1,23 @@
-﻿namespace Multi_PageWevAppwDB.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace Multi_PageWevAppwDB.Models
 {
     public class Contact
     {
+        //EF Core will configure the database to generte this value
+        public int ContactID { get; set; }
+
+        [Required(ErrorMessage ="Please enter a name.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter a phone number.")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter an address.")]
+        public string Address { get; set; } = string.Empty;
+
+        [StringLength(100, ErrorMessage ="Note cannot exceed 100 characters.")]
+        public string Note { get; set; } = string.Empty;
+
     }
 }
